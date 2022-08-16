bf269443b23b04bc4d0aae9090c9a2ff57ccb3a2
Sabita Kumari <sabitakumari3175@gmail.com>
2022-08-16 15:46:13 +0530

    digita clock-by using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const DgitalClock = () => {
+  let time = new Date().toLocaleTimeString();
+    
+  const [ctime, setCtime] = useState(time)
 
-export default App;
+  const UpdateTime = () => {
+    let time = new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+  setInterval(UpdateTime, 1000);
+  return (
+    <>
+      <div className='digitalC'>
+      <h1>{time }</h1>
+      <button onClick={UpdateTime}>DIGITAL-CLOCK</button>
+      </div>
+    </>
+  )
+};
+export default DgitalClock;
